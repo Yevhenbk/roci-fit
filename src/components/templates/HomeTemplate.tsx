8668d727d1f6046552b91670f2f62f38545db2a9
Yevhenbk <balagutrak.jenia@gmail.com>
2023-03-26 22:42:05 +0200

    configure template

--- src/components/templates/HomeTemplate.tsx
+++ src/components/templates/HomeTemplate.tsx
@@ -1,16 +1,29 @@
 import { FC, useContext } from 'react'
 import { Context } from '@store/context'
 import Button from '@UI/Button/Button'
+import FadeInContainer from '@UI/FadeInContainer/FadeInContainer'
 
 const HomeTemplate: FC = () => {
-  const { test } = useContext(Context)    
+  const { fadeIn, setFadeIn } = useContext(Context)    
 
   return (
-    <>
-      <Button intent='secondary'>
-        {test}
-      </Button>  
-    </>
+    <div className='flex flex-col gap-8'>
+      <FadeInContainer duration={4}>
+        <h1 className="text-3xl font-bold mb-4">
+          Welcome to Rocio's Workout App!
+        </h1>
+      </FadeInContainer>
+      <FadeInContainer duration={3}>
+        <Button intent='secondary'>
+          Button
+        </Button>
+      </FadeInContainer>  
+      <FadeInContainer duration={2}>
+        <Button intent='secondary'>
+          Button
+        </Button>
+      </FadeInContainer>  
+    </div>
   )
 }
 
