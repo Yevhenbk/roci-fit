a22f03948998e24e731dd69af03c05a0656d0188
Yevhenbk <balagutrak.jenia@gmail.com>
2023-03-27 19:59:10 +0200

    update buttons ui

--- src/components/templates/HomeTemplate.tsx
+++ src/components/templates/HomeTemplate.tsx
@@ -1,5 +1,6 @@
-import { FC, useContext, Fragment } from 'react'
+import { FC, useContext } from 'react'
 import { Context } from '@store/context'
+import { BsArrowRight } from 'react-icons/bs'
 import Button from '@UI/Button/Button'
 import FadeInContainer from '@UI/FadeInContainer/FadeInContainer'
 
@@ -25,22 +26,30 @@ const HomeTemplate: FC = () => {
       <div className='flex flex-col items-stretch gap-5 min-w-[22rem]'>
         <FadeInContainer duration={4}>
           <Button intent='secondary' fullWidth={true}>
-            <p className='font-medium'>Legs</p>
+            <p className='font-medium pl-4'>Legs</p>
+            <BsArrowRight className='relative duration-300 right-4 text-2xl 
+            group-hover:right-0'/>
           </Button>
         </FadeInContainer>
         <FadeInContainer duration={3}>
           <Button fullWidth={true}>
-            <p className='font-medium'>Back & Arms</p>
+            <p className='font-medium pl-4'>Back & Arms</p>
+            <BsArrowRight className='relative duration-300 right-4 text-2xl 
+            group-hover:right-0'/>
           </Button>
         </FadeInContainer>  
         <FadeInContainer duration={2}>
           <Button intent='teritary' fullWidth={true}>
-            <p className='font-medium'>Chest & Shoulders</p>
+            <p className='font-medium pl-4'>Chest & Shoulders</p>
+            <BsArrowRight className='relative duration-300 right-4 text-2xl 
+            group-hover:right-0'/>
           </Button>
         </FadeInContainer>  
         <FadeInContainer duration={1}>
           <Button intent='quaternary' fullWidth={true}>
-            <p className='font-medium'>Abs</p>
+            <p className='font-medium pl-4'>Abs</p>
+            <BsArrowRight className='relative duration-300 right-4 text-2xl 
+            group-hover:right-0'/>
           </Button>
         </FadeInContainer>  
       </div>
