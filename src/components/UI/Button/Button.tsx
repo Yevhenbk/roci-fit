7e3d7028522ea2c5572ea783de801c90b87e4ee4
Yevhenbk <balagutrak.jenia@gmail.com>
2023-03-27 19:04:59 +0200

    add onClick prop

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -1,9 +1,10 @@
-import { FC, ReactNode } from 'react'
+import { FC, ReactNode, MouseEventHandler } from 'react'
 import { VariantProps } from 'class-variance-authority'
 import { buttonStyles } from './ButtonStyled'
 
 interface Props extends 
   VariantProps<typeof buttonStyles> {
+    onClick?: MouseEventHandler<HTMLButtonElement>,
     children: ReactNode
   }  
 
@@ -11,6 +12,7 @@ const Button: FC<Props> = ({ intent, fullWidth, ...props }) => {
   return (
     <button
       className={buttonStyles({ intent, fullWidth })}
+      onClick={props.onClick}
     >
       {props.children}
     </button>
