08a5b9636779baaee731b8739322a0aa027443dd
Yevhenbk <balagutrak.jenia@gmail.com>
2023-03-27 19:05:14 +0200

    add intent

--- src/components/UI/Button/ButtonStyled.ts
+++ src/components/UI/Button/ButtonStyled.ts
@@ -1,15 +1,17 @@
 import { cva } from 'class-variance-authority'
 
 export const buttonStyles = cva(
-  'text-xl',
+  'text-xl rounded-full p-4',
   {
     variants: {
       intent: {
-        primary: 'text-blue-500',
-        secondary: 'bg-orange-800 p-4 text-white h-5rem'
+        primary: 'bg-[#A4D2EB] text-[#57A6CA]',
+        secondary: 'bg-[#FEAFE7] text-pink-700',
+        teritary: 'bg-[#CFABF8] text-white',
+        quaternary: 'bg-[#B3CB6C] text-green-700'
       },
       fullWidth: {
-        true: 'w-[100vw]'
+        true: 'w-[100%]'
       }  
     },
     defaultVariants: {
