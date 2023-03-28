d0b1f0f76fecdb9b47890de6dd2d398c1358f1c6
Yevhenbk <balagutrak.jenia@gmail.com>
2023-03-28 13:26:26 +0200

    add margin

--- src/components/UI/ContentWrapper/ContentWrapper.tsx
+++ src/components/UI/ContentWrapper/ContentWrapper.tsx
@@ -12,7 +12,7 @@ const ContentWrapper: FC<Props> = (props) => {
 
   return (
     <div className={classNames('opacity-0 transform transition ease-out flex flex-col gap-14',
-    'duration-300 justify-start items-center h-[100vh] w-[100vw] overflow-auto py-12', {
+    'duration-300 justify-start items-center h-[100vh] w-[100vw] overflow-auto py-12 mb-12', {
     'opacity-100 translate-y-0': !section,
     'translate-y-6': section})}>
       {props.children}
